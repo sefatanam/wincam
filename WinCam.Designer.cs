dd653b43fc1b011a853a5a8f13f42e1c25fd3afd
Sefat Anam <sefatanam@gmail.com>
2020-06-07 02:48:22 +0600

    Access any ip/web-cam by ip address

---
+++ WinCam.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WinCam
+{
+    partial class WinCam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            VisioForge.Types.VideoRendererSettingsWinForms videoRendererSettingsWinForms1 = new VisioForge.Types.VideoRendererSettingsWinForms();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinCam));
+            this.videoCapture1 = new VisioForge.Controls.UI.WinForms.VideoCapture();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ipLabel = new System.Windows.Forms.Label();
+            this.ipAddressTextBox = new System.Windows.Forms.TextBox();
+            this.SetButton = new System.Windows.Forms.Button();
+            this.AddNewIpButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoCapture1
+            // 
+            this.videoCapture1.Additional_Audio_CaptureDevice_MixChannels = false;
+            this.videoCapture1.Audio_CaptureDevice = "";
+            this.videoCapture1.Audio_CaptureDevice_CustomLatency = 0;
+            this.videoCapture1.Audio_CaptureDevice_Format = "";
+            this.videoCapture1.Audio_CaptureDevice_Format_UseBest = true;
+            this.videoCapture1.Audio_CaptureDevice_Line = "";
+            this.videoCapture1.Audio_CaptureDevice_MasterDevice = null;
+            this.videoCapture1.Audio_CaptureDevice_MasterDevice_Format = null;
+            this.videoCapture1.Audio_CaptureDevice_Path = null;
+            this.videoCapture1.Audio_CaptureSourceFilter = null;
+            this.videoCapture1.Audio_Channel_Mapper = null;
+            this.videoCapture1.Audio_Decoder = null;
+            this.videoCapture1.Audio_Effects_Enabled = false;
+            this.videoCapture1.Audio_Effects_UseLegacyEffects = false;
+            this.videoCapture1.Audio_Enhancer_Enabled = false;
+            this.videoCapture1.Audio_OutputDevice = "Default DirectSound Device";
+            this.videoCapture1.Audio_PCM_Converter = null;
+            this.videoCapture1.Audio_PlayAudio = true;
+            this.videoCapture1.Audio_RecordAudio = true;
+            this.videoCapture1.Audio_Sample_Grabber_Enabled = false;
+            this.videoCapture1.Audio_VUMeter_Enabled = false;
+            this.videoCapture1.Audio_VUMeter_Pro_Enabled = false;
+            this.videoCapture1.Audio_VUMeter_Pro_Volume = 100;
+            this.videoCapture1.BackColor = System.Drawing.Color.Black;
+            this.videoCapture1.Barcode_Reader_Enabled = false;
+            this.videoCapture1.Barcode_Reader_Type = VisioForge.Types.VFBarcodeType.Auto;
+            this.videoCapture1.BDA_Source = null;
+            this.videoCapture1.ChromaKey = null;
+            this.videoCapture1.Custom_Source = null;
+            this.videoCapture1.CustomRedist_Enabled = false;
+            this.videoCapture1.CustomRedist_Path = null;
+            this.videoCapture1.Debug_Dir = "";
+            this.videoCapture1.Debug_Mode = false;
+            this.videoCapture1.Debug_Telemetry = false;
+            this.videoCapture1.Decklink_Input = VisioForge.Types.DecklinkInput.Auto;
+            this.videoCapture1.Decklink_Input_Capture_Timecode_Source = VisioForge.Types.DecklinkCaptureTimecodeSource.Auto;
+            this.videoCapture1.Decklink_Input_IREUSA = false;
+            this.videoCapture1.Decklink_Input_SMPTE = false;
+            this.videoCapture1.Decklink_Output = null;
+            this.videoCapture1.Decklink_Source = null;
+            this.videoCapture1.DirectCapture_Muxer = null;
+            this.videoCapture1.DV_Decoder_Video_Resolution = VisioForge.Types.VFDVVideoResolution.Full;
+            this.videoCapture1.Face_Tracking = null;
+            this.videoCapture1.IP_Camera_Source = null;
+            this.videoCapture1.Location = new System.Drawing.Point(36, 81);
+            this.videoCapture1.Mode = VisioForge.Types.VFVideoCaptureMode.VideoPreview;
+            this.videoCapture1.Motion_Detection = null;
+            this.videoCapture1.Motion_DetectionEx = null;
+            this.videoCapture1.MPEG_Audio_Decoder = "";
+            this.videoCapture1.MPEG_Demuxer = null;
+            this.videoCapture1.MPEG_Video_Decoder = "";
+            this.videoCapture1.MultiScreen_Enabled = false;
+            this.videoCapture1.Name = "videoCapture1";
+            this.videoCapture1.Network_Streaming_Audio_Enabled = false;
+            this.videoCapture1.Network_Streaming_Enabled = false;
+            this.videoCapture1.Network_Streaming_Format = VisioForge.Types.VFNetworkStreamingFormat.WMV;
+            this.videoCapture1.Network_Streaming_Network_Port = 100;
+            this.videoCapture1.Network_Streaming_Output = null;
+            this.videoCapture1.Network_Streaming_URL = "";
+            this.videoCapture1.Network_Streaming_WMV_Maximum_Clients = 10;
+            this.videoCapture1.Output_Filename = "C:\\Users\\Sefat\\Documents\\VisioForge\\output.avi";
+            this.videoCapture1.Output_Format = null;
+            this.videoCapture1.PIP_AddSampleGrabbers = false;
+            this.videoCapture1.PIP_ChromaKeySettings = null;
+            this.videoCapture1.PIP_Mode = VisioForge.Types.VFPIPMode.Custom;
+            this.videoCapture1.PIP_ResizeQuality = VisioForge.Types.VFPIPResizeQuality.RQ_NN;
+            this.videoCapture1.Push_Source = null;
+            this.videoCapture1.Screen_Capture_Source = null;
+            this.videoCapture1.SeparateCapture_AutostartCapture = false;
+            this.videoCapture1.SeparateCapture_Enabled = false;
+            this.videoCapture1.SeparateCapture_Filename_Mask = "output %yyyy-%MM-%dd %hh-%mm-%ss.%ext";
+            this.videoCapture1.SeparateCapture_FileSizeThreshold = ((long)(0));
+            this.videoCapture1.SeparateCapture_GMFMode = true;
+            this.videoCapture1.SeparateCapture_Mode = VisioForge.Types.VFSeparateCaptureMode.Normal;
+            this.videoCapture1.SeparateCapture_TimeThreshold = ((long)(0));
+            this.videoCapture1.Size = new System.Drawing.Size(882, 433);
+            this.videoCapture1.Start_DelayEnabled = false;
+            this.videoCapture1.TabIndex = 0;
+            this.videoCapture1.Tags = null;
+            this.videoCapture1.Timeshift_Settings = null;
+            this.videoCapture1.TVTuner_Channel = 0;
+            this.videoCapture1.TVTuner_Country = "";
+            this.videoCapture1.TVTuner_FM_Tuning_StartFrequency = 80000000;
+            this.videoCapture1.TVTuner_FM_Tuning_Step = 160000000;
+            this.videoCapture1.TVTuner_FM_Tuning_StopFrequency = 0;
+            this.videoCapture1.TVTuner_Frequency = 0;
+            this.videoCapture1.TVTuner_InputType = "";
+            this.videoCapture1.TVTuner_Mode = VisioForge.Types.VFTVTunerMode.Default;
+            this.videoCapture1.TVTuner_Name = "";
+            this.videoCapture1.TVTuner_TVFormat = VisioForge.Types.VFTVTunerVideoFormat.PAL_D;
+            this.videoCapture1.Video_CaptureDevice = "";
+            this.videoCapture1.Video_CaptureDevice_Format = "";
+            this.videoCapture1.Video_CaptureDevice_Format_UseBest = true;
+            this.videoCapture1.Video_CaptureDevice_FrameRate = 25D;
+            this.videoCapture1.Video_CaptureDevice_InternalMPEGEncoder_Name = "";
+            this.videoCapture1.Video_CaptureDevice_IsAudioSource = false;
+            this.videoCapture1.Video_CaptureDevice_Path = null;
+            this.videoCapture1.Video_CaptureDevice_UseClosedCaptions = false;
+            this.videoCapture1.Video_CaptureDevice_UseRAWSampleGrabber = false;
+            this.videoCapture1.Video_Crop = null;
+            this.videoCapture1.Video_Decoder = null;
+            this.videoCapture1.Video_Effects_AllowMultipleStreams = false;
+            this.videoCapture1.Video_Effects_Enabled = false;
+            videoRendererSettingsWinForms1.Aspect_Ratio_Override = false;
+            videoRendererSettingsWinForms1.Aspect_Ratio_X = 16;
+            videoRendererSettingsWinForms1.Aspect_Ratio_Y = 9;
+            videoRendererSettingsWinForms1.BackgroundColor = System.Drawing.Color.Black;
+            videoRendererSettingsWinForms1.Deinterlace_EVR_Mode = VisioForge.Types.EVRDeinterlaceMode.Auto;
+            videoRendererSettingsWinForms1.Deinterlace_VMR9_Mode = null;
+            videoRendererSettingsWinForms1.Deinterlace_VMR9_UseDefault = true;
+            videoRendererSettingsWinForms1.Flip_Horizontal = false;
+            videoRendererSettingsWinForms1.Flip_Vertical = false;
+            videoRendererSettingsWinForms1.RotationAngle = 0;
+            videoRendererSettingsWinForms1.StretchMode = VisioForge.Types.VFVideoRendererStretchMode.Letterbox;
+            videoRendererSettingsWinForms1.Video_Renderer = VisioForge.Types.VFVideoRenderer.EVR;
+            videoRendererSettingsWinForms1.VideoRendererInternal = VisioForge.Types.VFVideoRendererInternal.EVR;
+            videoRendererSettingsWinForms1.Zoom_Ratio = 0;
+            videoRendererSettingsWinForms1.Zoom_ShiftX = 0;
+            videoRendererSettingsWinForms1.Zoom_ShiftY = 0;
+            this.videoCapture1.Video_Renderer = videoRendererSettingsWinForms1;
+            this.videoCapture1.Video_Resize = null;
+            this.videoCapture1.Video_ResizeOrCrop_Enabled = false;
+            this.videoCapture1.Video_Rotation = VisioForge.Types.VFRotateMode.RotateNone;
+            this.videoCapture1.Video_Sample_Grabber_Enabled = true;
+            this.videoCapture1.Video_Sample_Grabber_UseForVideoEffects = true;
+            this.videoCapture1.Video_Still_Frames_Grabber_Enabled = false;
+            this.videoCapture1.Virtual_Camera_Output_Enabled = false;
+            this.videoCapture1.Virtual_Camera_Output_LicenseKey = null;
+            this.videoCapture1.VLC_Path = null;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.StartButton.Location = new System.Drawing.Point(321, 537);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(97, 41);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.StopButton.Location = new System.Drawing.Point(438, 537);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(85, 41);
+            this.StopButton.TabIndex = 2;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.ForeColor = System.Drawing.Color.Red;
+            this.ExitButton.Location = new System.Drawing.Point(545, 537);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(85, 41);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ipLabel
+            // 
+            this.ipLabel.AutoSize = true;
+            this.ipLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ipLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.ipLabel.Location = new System.Drawing.Point(98, 40);
+            this.ipLabel.Name = "ipLabel";
+            this.ipLabel.Size = new System.Drawing.Size(136, 20);
+            this.ipLabel.TabIndex = 3;
+            this.ipLabel.Text = "Enter IP Address :";
+            // 
+            // ipAddressTextBox
+            // 
+            this.ipAddressTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipAddressTextBox.Location = new System.Drawing.Point(240, 38);
+            this.ipAddressTextBox.Name = "ipAddressTextBox";
+            this.ipAddressTextBox.Size = new System.Drawing.Size(482, 24);
+            this.ipAddressTextBox.TabIndex = 4;
+            // 
+            // SetButton
+            // 
+            this.SetButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SetButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.SetButton.Location = new System.Drawing.Point(728, 36);
+            this.SetButton.Name = "SetButton";
+            this.SetButton.Size = new System.Drawing.Size(75, 26);
+            this.SetButton.TabIndex = 5;
+            this.SetButton.Text = "Set";
+            this.SetButton.UseVisualStyleBackColor = true;
+            this.SetButton.Click += new System.EventHandler(this.SetButton_Click);
+            // 
+            // AddNewIpButton
+            // 
+            this.AddNewIpButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNewIpButton.ForeColor = System.Drawing.Color.SteelBlue;
+            this.AddNewIpButton.Location = new System.Drawing.Point(839, 19);
+            this.AddNewIpButton.Name = "AddNewIpButton";
+            this.AddNewIpButton.Size = new System.Drawing.Size(103, 32);
+            this.AddNewIpButton.TabIndex = 5;
+            this.AddNewIpButton.Text = "Add New IP";
+            this.AddNewIpButton.UseVisualStyleBackColor = true;
+            this.AddNewIpButton.Click += new System.EventHandler(this.AddNewIpButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.AddNewIpButton);
+            this.groupBox1.Controls.Add(this.SetButton);
+            this.groupBox1.Controls.Add(this.ipAddressTextBox);
+            this.groupBox1.Controls.Add(this.ipLabel);
+            this.groupBox1.Controls.Add(this.ExitButton);
+            this.groupBox1.Controls.Add(this.StopButton);
+            this.groupBox1.Controls.Add(this.StartButton);
+            this.groupBox1.Controls.Add(this.videoCapture1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.groupBox1.Location = new System.Drawing.Point(12, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(948, 599);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Broadcast Your IP/WebCam";
+            // 
+            // WinCam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(977, 632);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "WinCam";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinCam";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VisioForge.Controls.UI.WinForms.VideoCapture videoCapture1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label ipLabel;
+        private System.Windows.Forms.TextBox ipAddressTextBox;
+        private System.Windows.Forms.Button SetButton;
+        private System.Windows.Forms.Button AddNewIpButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WinCam.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinCam
+{
+    public partial class WinCam : Form
+    {
+        public WinCam()
+        {
+            InitializeComponent();
+            StartButton.Hide();
+            StopButton.Hide();
+            ExitButton.Hide();
+            AddNewIpButton.Show();
+
+        }
+
+        private string IpAddress { get; set; }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(IpAddress))
+                {
+                    StartButton.Text = "Start";
+                    videoCapture1.IP_Camera_Source = new VisioForge.Types.Sources.IPCameraSourceSettings()
+                    { URL = $"{IpAddress}", Type = VisioForge.Types.VFIPSource.Auto_LAV }; /*like this type:-http://192.168.233.129:8000/camera/mjpeg*/
+                    videoCapture1.Audio_PlayAudio = videoCapture1.Audio_RecordAudio = false;
+                    videoCapture1.Mode = VisioForge.Types.VFVideoCaptureMode.IPPreview;
+                    videoCapture1.Start();
+
+                    //here need a validation, sometimes if IP goes offline it occured error
+                }
+                else
+                {
+                    StartButton.Text = "Not Ready";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong. Check Ip Address again.");
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                videoCapture1.Stop();
+            }
+            catch
+            {
+
+                MessageBox.Show("Something went wrong.");
+            }
+
+        }
+
+        private void SetButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //http:192.168.0.100 if need to validate this as entried IP_Address,here the right place. Put Here Your Code.
+
+
+                if (!String.IsNullOrEmpty(ipAddressTextBox.Text))
+                {
+                    IpAddress = ipAddressTextBox.Text;
+                    MessageBox.Show("Ip Address entried successfully");
+                    StartButton.Show();
+                    StopButton.Show();
+                    ExitButton.Show();
+                    AddNewIpButton.Show();
+                    ipLabel.Hide();
+                    ipAddressTextBox.Hide();
+                    SetButton.Hide();
+                }
+                else if (String.IsNullOrEmpty(ipAddressTextBox.Text))
+                {
+                    MessageBox.Show("Ip is not provided or empty.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong.");
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            try { Close(); } catch { MessageBox.Show("Something went wrong."); }
+
+        }
+
+        private void AddNewIpButton_Click(object sender, EventArgs e)
+        {
+
+            videoCapture1.Stop();
+            ipLabel.Show();
+            ipAddressTextBox.Show();
+            SetButton.Show();
+            StartButton.Hide();
+            StopButton.Hide();
+            ExitButton.Hide();
+            AddNewIpButton.Hide();
+
+        }
+    }
+}
